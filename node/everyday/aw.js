064db035a4c9ee15a7680580b23abafe5d285c58
PaulZeng <510830568@qq.com>
2017-05-14 19:04:49 +0800

    Auto-commit

--- node/everyday/aw.js
+++ node/everyday/aw.js
@@ -20,50 +20,39 @@ x('http://txly2.net/aw', 'tbody tr', {
         audio.time = audio.time.substring(audio.time.lastIndexOf('-') + 1);
         download(audio.downUrl).then(data => {
             fs.writeFileSync('../../aw/' + fileName, data);
+
+            audio.duration = 3560;
+            audio.size = "28.5M";
+            audio.artistId = 31;
+            audio.artistName = "空中崇拜";
+            audio.id = 31000000 + parseInt(audio.time.substring(2), 10);
+
+            fs.writeFileSync("./aw.json", JSON.stringify(audio, null, '\t'));
+
+            if (!shell.which('git')) {
+                shell.echo('Sorry, this script requires git');
+                shell.exit(1);
+            }
+
+            if (shell.exec('git add .').code !== 0) {
+                shell.echo('Error: Git add failed');
+                shell.exit(1);
+            }
+
+            if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
+                shell.echo('Error: Git commit failed');
+                shell.exit(1);
+            }
+
+            if (shell.exec('git tag aw' + audio.time).code !== 0) {
+                shell.echo('Error: Git tag failed');
+                shell.exit(1);
+            }
+
+            if (shell.exec('git push –tags').code !== 0) {
+                shell.echo('Error: Git push failed');
+                shell.exit(1);
+            }
         });
-        audio.duration = 3560;
-        audio.size = "28.5M";
-        audio.artistId = 31;
-        audio.artistName = "空中崇拜";
-        audio.id = 31000000 + parseInt(audio.time.substring(2), 10);
-
-        fs.writeFileSync("./aw.json", JSON.stringify(audio, null, '\t'));
-
-
-        if (!shell.which('git')) {
-            shell.echo('Sorry, this script requires git');
-            shell.exit(1);
-        }
-
-        // shell.rm('-rf', 'out/Release');
-        // shell.cp('-R', 'stuff/', 'out/Release');
-
-        // shell.cd('lib');
-        // shell.ls('*.js').forEach(function(file) {
-        //     shell.sed('-i', 'v0.1.2', 'v0.1.2', file);
-
-        //     shell.sed('-i', /.*REPLACE_LINE_WITH_MACRO.*\n/, shell.cat('macro.js'), file);
-        // });
-        // shell.cd('..');
-
-        if (shell.exec('git add .').code !== 0) {
-            shell.echo('Error: Git add failed');
-            shell.exit(1);
-        }
-
-        if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
-            shell.echo('Error: Git commit failed');
-            shell.exit(1);
-        }
-
-
-        if (shell.exec('git tag aw' + audio.time).code !== 0) {
-            shell.echo('Error: Git tag failed');
-            shell.exit(1);
-        }
-
-        if (shell.exec('git push').code !== 0) {
-            shell.echo('Error: Git push failed');
-            shell.exit(1);
-        }
+        
     });
